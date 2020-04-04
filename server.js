4f37fc0ff8f1cd625114c697355c28d69b70cf78
Abbosjon <squarefozilov@gmail.com>
2020-04-03 23:55:28 -0400

    <message>

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(
   })
 )
 
-const mongoURI = 'mongodb://localhost:27017/mernloginreg'
+const mongoURI = 'mongodb://localhost:27017/mernloginreg' || process.env.MONGODB_URI;
 
 mongoose
   .connect(
